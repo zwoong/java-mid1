22eda3c2d94a1a9f6b377bd346d0c75ee97e0365
zwoong <zwoong6867@gmail.com>
2025-04-09 22:46:02 +0900

    ✨ RectangleMain

---
+++ src/lang/object/test/Rectangle.java
@@ -0,0 +1,27 @@
+package lang.object.test;
+
+public class Rectangle {
+    private int width;
+    private int height;
+
+    public Rectangle(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "width=" + width +
+                ", height=" + height +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Rectangle rectangle = (Rectangle) o;
+        return width == rectangle.width && height == rectangle.height;
+    }
+
+}
