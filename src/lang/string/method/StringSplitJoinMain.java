814805dc3df9cc3e2e43683b4de60e4801790e5b
zwoong <zwoong6867@gmail.com>
2025-04-10 18:53:26 +0900

    ✨ StringUtilsMain2

---
+++ src/lang/string/method/StringSplitJoinMain.java
@@ -0,0 +1,32 @@
+package lang.string.method;
+
+public class StringSplitJoinMain {
+
+    public static void main(String[] args) {
+        String str = "Apple,Banana,Orange";
+
+        // split()
+        String[] splitStr = str.split(",");
+        for (String s : splitStr) {
+            System.out.println(s);
+        }
+
+        String joinStr = "";
+        for (int i=0; i < splitStr.length; i++) {
+            String string = splitStr[i];
+            joinStr += string;
+            if (i != splitStr.length - 1) {
+                joinStr += "-";
+            }
+        }
+
+        System.out.println("joinStr = " + joinStr);
+        // join()
+        String joinedStr = String.join("-", "A", "B", "C");
+        System.out.println("연결된 문자열 = " + joinedStr);
+
+        // 문자열 배열 연결
+        String result = String.join("-", splitStr);
+        System.out.println("result = " + result);
+    }
+}
