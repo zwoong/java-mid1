ea91072fb08ac1d1decfbd489c141ac3b83e37d0
zwoong <zwoong6867@gmail.com>
2025-04-11 10:31:11 +0900

    ✨ ClassGradeRefMain1

---
+++ src/enumeration/ref1/DiscountService.java
@@ -0,0 +1,9 @@
+package enumeration.ref1;
+
+public class DiscountService {
+
+  public int discount(ClassGrade classGrade, int price) {
+    return price * classGrade.getDiscountPercent() / 100;
+  }
+
+}
