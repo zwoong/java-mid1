38402bc54378060d969f2393c80d37c8b6cd24a3
zwoong <zwoong6867@gmail.com>
2025-04-11 09:40:55 +0900

    ✨ ClassGradeEx2_2

---
+++ src/enumeration/ex2/ClassGradeEx2_2.java
@@ -0,0 +1,14 @@
+package enumeration.ex2;
+
+public class ClassGradeEx2_2 {
+
+  public static void main(String[] args) {
+    int price = 10000;
+
+    DiscountService discountService = new DiscountService();
+    /*ClassGrade newClassGrade = new ClassGrade();
+    int result = discountService.discount(newClassGrade, price);
+    System.out.println("result = " + result);*/
+  }
+
+}
