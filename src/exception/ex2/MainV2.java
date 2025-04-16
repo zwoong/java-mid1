b765a26424b34e3e070418f5ad5861b791ad663b
zwoong <zwoong6867@gmail.com>
2025-04-16 12:47:27 +0900

    ✨ NetworkServiceV2_5

--- src/exception/ex2/MainV2.java
+++ src/exception/ex2/MainV2.java
@@ -7,7 +7,8 @@ public class MainV2 {
   public static void main(String[] args) throws NetworkClientExceptionV2 {
     // NetworkServiceV2_1 networkService = new NetworkServiceV2_1();
     // NetworkServiceV2_2 networkService = new NetworkServiceV2_2();
-    NetworkServiceV2_4 networkService = new NetworkServiceV2_4();
+    // NetworkServiceV2_4 networkService = new NetworkServiceV2_4();
+    NetworkServiceV2_5 networkService = new NetworkServiceV2_5();
 
     Scanner scanner = new Scanner(System.in);
     while (true) {
