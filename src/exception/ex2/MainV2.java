08b76bbbd92d6edbd86501eb50a729b97f2b522e
zwoong <zwoong6867@gmail.com>
2025-04-16 11:51:50 +0900

    ✨ NetworkServiceV2_4

--- src/exception/ex2/MainV2.java
+++ src/exception/ex2/MainV2.java
@@ -7,8 +7,8 @@ public class MainV2 {
   public static void main(String[] args) throws NetworkClientExceptionV2 {
     // NetworkServiceV2_1 networkService = new NetworkServiceV2_1();
     // NetworkServiceV2_2 networkService = new NetworkServiceV2_2();
-    NetworkServiceV2_3 networkService = new NetworkServiceV2_3();
-    
+    NetworkServiceV2_4 networkService = new NetworkServiceV2_4();
+
     Scanner scanner = new Scanner(System.in);
     while (true) {
       System.out.print("전송할 문자: ");
