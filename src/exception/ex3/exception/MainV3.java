857610008525abe811deec93aa0c80d60d897bff
zwoong <zwoong6867@gmail.com>
2025-04-16 14:08:25 +0900

    ✨ NetworkServiceV3_2

--- src/exception/ex3/exception/MainV3.java
+++ src/exception/ex3/exception/MainV3.java
@@ -5,8 +5,9 @@ import java.util.Scanner;
 public class MainV3 {
 
   public static void main(String[] args) {
-    NetworkServiceV3_1 networkService = new NetworkServiceV3_1();
-
+    // NetworkServiceV3_1 networkService = new NetworkServiceV3_1();
+    NetworkServiceV3_2 networkService = new NetworkServiceV3_2();
+    
     Scanner scanner = new Scanner(System.in);
     while (true) {
       System.out.print("전송할 문자: ");
